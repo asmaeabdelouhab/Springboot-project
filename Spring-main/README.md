996c803230620dea4dca82b34279f62a854af2bd
asmaeabdelouhab <asmae_abdelouhab@um5.ac.ma>
2024-10-23 16:20:57 +0100

    first

---
+++ Spring-main/README.md
@@ -0,0 +1,1 @@
+# Basic-Spring JAVA 17
---
+++ Spring-main/src/main/java/com/ev/pcs/fullstack/FullstackFirstSeanceApplication.java
@@ -0,0 +1,13 @@
+package com.ev.pcs.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackFirstSeanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackFirstSeanceApplication.class, args);
+    }
+
+}
